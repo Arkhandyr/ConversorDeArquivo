bc440aa44b0490f6ac5bf33dff25e69babe12ecf
Arkhandyr <Arkhandyr@DESKTOP-4IM4VCG>
2021-10-21 14:20:37 -0300

    Add project files.

---
+++ ConversorDeArquivo/ConversorPDF.cs
@@ -0,0 +1,37 @@
+﻿
+using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConversorDeArquivo
+{
+    public class ConversorPDF
+    {
+        public static void GerarPDF(FileStream txt)
+        {
+            var writer = new PdfWriter();
+            var pdfDocument = new PdfDocument(writer);
+            var pdf = new Document(pdfDocument);
+
+            #region Estilos
+            PdfFont fontCorpo = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+            Style helvetica24r = new();
+            helvetica24r.SetFont(fontCorpo).SetFontSize(24);
+            #endregion
+
+            #region Parágrafos
+            Paragraph body = new Paragraph().SetTextAlignment(TextAlignment.LEFT).AddStyle(helvetica24r);
+            #endregion
+
+            pdf.Add(body);
+
+            pdf.Close();
+        }
+    }
+}
---
+++ ConversorDeArquivo/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace ConversorDeArquivo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging(config => config.AddSerilog(Log.Logger));
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
